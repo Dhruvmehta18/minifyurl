ff6984f1b167d55836e0cf7e47dd4540c89f3a6f
Dhruv Mehta <39973939+Dhruvmehta18@users.noreply.github.com>
2021-03-20 11:05:04 +0000

    updated hash.route.route

--- minify-backend-user/src/routes/v1/hash.route.js
+++ minify-backend-user/src/routes/v1/hash.route.js
@@ -7,5 +7,7 @@ const router = express.Router();
 
 router.post('/minify', validate(hashValidation.createHashUrl), hashController.createUrl);
 router.get('/:minify_id', hashController.getUrl);
+router.put('/minify-originalLink', hashController.updateUrl);
+router.delete('/minify-delete', hashController.deleteUrl);
 
 module.exports = router;
