ff975a8284eeecad9d2ffec7b7d5d0383ca8ae9e
Dhruvmehta18 <20.09.2004.bh@gmail.com>
2022-05-06 08:17:50 +0530

    qrcode integrated

--- frontend-user/component/MinifyListItemDetail/index.js
+++ frontend-user/component/MinifyListItemDetail/index.js
@@ -1,4 +1,5 @@
-import React, { memo, useEffect, useState } from "react";
+import React, { memo, useCallback, useEffect, useState } from "react";
+import { QRCodeCanvas } from "qrcode.react";
 import styles from "../../styles/Home.module.scss";
 import { getMinifyDetail } from "../../redux/selectors";
 import { useDispatch, useSelector } from "react-redux";
@@ -8,6 +9,7 @@ import getRedirectUrl from "../../lib/utility/getRedirectUrl";
 import { REDIRECT_SERVICE_URL } from "../../lib/config/config";
 import PerformanceChart from "../PerformanceChart";
 import PieChart from "../PieChart";
+import useToggle from "../../hooks/useToggle";
 
 const index = ({ minifyIdentity }) => {
   const dispatch = useDispatch();
@@ -15,12 +17,17 @@ const index = ({ minifyIdentity }) => {
   const { requestState, data: minifyData } = minifyDetail;
 
   const [teleRefSplitUp, setTeleRefSplitUp] = useState([]);
+  const [isQrcodeDisplay, toggleIsQrCodeDisplay] = useToggle();
 
   const onPerformanceDataFetch = (data) => {
     const { telemetryRefererSplitUp } = data;
     console.log(telemetryRefererSplitUp);
     setTeleRefSplitUp([...telemetryRefererSplitUp]);
   };
+
+  const onQrButtonClicked = useCallback(()=>{
+    toggleIsQrCodeDisplay();
+  }, [isQrcodeDisplay]);
 
   useEffect(() => {
     if (minifyIdentity && minifyIdentity !== "") {
@@ -31,77 +38,91 @@ const index = ({ minifyIdentity }) => {
   return (
     <div className={styles.linkData}>
       {minifyIdentity && minifyIdentity !== "" && requestState === LOADED && (
-        <div>
-          <div className={styles.mainData}>
-            <div className={styles.mainDataTopRow}>
-              <time dateTime="2021-10-27">{`CREATED ${minifyData.creationTime}`}</time>
-              <span>
-                <span className={styles.infoWrapper_Divider}>|</span>
-                <span className="item-detail--created-link">
-                  {minifyData.userId}
+        <>
+          {isQrcodeDisplay && (
+            <div className={styles.qrcodeContainer}>
+              <div className={styles.qrcodeWrapper} onClick={onQrButtonClicked}>
+                <div className={styles.qrcodeElementWrapper}>
+                  <QRCodeCanvas
+                    value={getRedirectUrl(minifyData.minifyId)}
+                    size={200}
+                  />
+                </div>
+              </div>
+            </div>
+          )}
+          <div>
+            <div className={styles.mainData}>
+              <div className={styles.mainDataTopRow}>
+                <time dateTime="2021-10-27">{`CREATED ${minifyData.creationTime}`}</time>
+                <span>
+                  <span className={styles.infoWrapper_Divider}>|</span>
+                  <span className="item-detail--created-link">
+                    {minifyData.userId}
+                  </span>
                 </span>
-              </span>
-            </div>
-            <div className={styles.mainDataSecondRow}>
-              <h3 className={styles.linkDataTitle}>first</h3>
-              <small>
-                <a
-                  className={styles.linkDataOriginalLink}
-                  href={minifyData.originalLink}
-                  target="_blank"
-                >
-                  {minifyData.originalLink}
-                </a>
-              </small>
-            </div>
-            <div className={styles.mainDataBottomRow}>
-              <div className={styles.bottomRowItem}>
-                <a
-                  href={getRedirectUrl(minifyData.minifyId)}
-                  className={styles.shortLink}
-                  target="_blank"
-                >
-                  {REDIRECT_SERVICE_URL}/<b>{minifyData.minifyId}</b>
-                </a>
               </div>
-              <div className={styles.bottomRowItem}>
-                <button
-                  onClick={() => {
-                    navigator.clipboard.writeText(
-                      getRedirectUrl(minifyData.minifyId)
-                    );
-                  }}
-                >
-                  Copy
-                </button>
+              <div className={styles.mainDataSecondRow}>
+                <h3 className={styles.linkDataTitle}>first</h3>
+                <small>
+                  <a
+                    className={styles.linkDataOriginalLink}
+                    href={minifyData.originalLink}
+                    target="_blank"
+                  >
+                    {minifyData.originalLink}
+                  </a>
+                </small>
               </div>
+              <div className={styles.mainDataBottomRow}>
+                <div className={styles.bottomRowItem}>
+                  <a
+                    href={getRedirectUrl(minifyData.minifyId)}
+                    className={styles.shortLink}
+                    target="_blank"
+                  >
+                    {REDIRECT_SERVICE_URL}/<b>{minifyData.minifyId}</b>
+                  </a>
+                </div>
+                <div className={styles.bottomRowItem}>
+                  <button
+                    onClick={() => {
+                      navigator.clipboard.writeText(
+                        getRedirectUrl(minifyData.minifyId)
+                      );
+                    }}
+                  >
+                    Copy
+                  </button>
+                </div>
 
-              <div className={styles.bottomRowItem}>
-                <button>Edit</button>
-              </div>
+                <div className={styles.bottomRowItem}>
+                  <button>Edit</button>
+                </div>
 
-              <div className={styles.bottomRowItem}>
-                <button>Qr Code</button>
+                <div className={styles.bottomRowItem}>
+                  <button onClick={onQrButtonClicked}>Qr Code</button>
+                </div>
+              </div>
+            </div>
+            <div className={styles.clicksData}>
+              <PerformanceChart
+                LeftComponent={LeftClicksData}
+                type="detail"
+                minifyIdentity={minifyIdentity}
+                onFetch={onPerformanceDataFetch}
+              />
+            </div>
+            <div className={styles.referrerBlock}>
+              <div className={styles.referrerTitle}>
+                <h2>Referrer</h2>
+              </div>
+              <div>
+                <PieChart data={teleRefSplitUp} />
               </div>
             </div>
           </div>
-          <div className={styles.clicksData}>
-            <PerformanceChart
-              LeftComponent={LeftClicksData}
-              type="detail"
-              minifyIdentity={minifyIdentity}
-              onFetch={onPerformanceDataFetch}
-            />
-          </div>
-          <div className={styles.referrerBlock}>
-            <div className={styles.referrerTitle}>
-              <h2>Referrer</h2>
-            </div>
-            <div>
-              <PieChart data={teleRefSplitUp} />
-            </div>
-          </div>
-        </div>
+        </>
       )}
     </div>
   );
